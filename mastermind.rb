97b26c13377b0492f7e0f88c67a4925df06de973
JSE955 <jordanscottedwards@gmail.com>
2024-05-03 14:55:10 -0400

    Add Game class with basic features

--- mastermind.rb
+++ mastermind.rb
@@ -6,4 +6,39 @@ class Peg
   def initialize(color)
     @color = color
   end
+
+  def self.code_colors
+    CODE_COLORS
+  end
+
+  def self.key_colors
+    KEY_COLORS
+  end
 end
+
+class Game
+  attr_accessor :secret_code, :turns, :all_guesses, :guess, :feedback
+
+  def initialize
+    @turns = prompt_for_turns
+    @secret_code = generate_secret_code
+  end
+
+  private
+
+  def prompt_for_turns
+    puts 'Welcome to Mastermind. How many turns for this game?'
+    gets.chomp.to_i
+  end
+
+  def generate_secret_code
+    secret_code = []
+    4.times do
+      secret_code.push(Peg.new(Peg.code_colors.sample))
+    end
+    secret_code
+  end
+end
+
+game = Game.new
+p game.secret_code
