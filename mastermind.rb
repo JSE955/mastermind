96da20ea4db5e6b153609f5ec15a8f1997228b96
JSE955 <jordanscottedwards@gmail.com>
2024-05-03 14:46:18 -0400

    Refactor to use one class for all pegs

--- mastermind.rb
+++ mastermind.rb
@@ -1,5 +1,6 @@
-class CodePeg
-  COLORS = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+class Peg
+  CODE_COLORS = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']
+  KEY_COLORS = ['black', 'white']
   attr_reader :color
 
   def initialize(color)
